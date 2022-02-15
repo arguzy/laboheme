9cdc60e859b2dd36bbfb195b4b725f0ae4886eaf
arguzy <arielzybala@gmail.com>
2022-02-15 15:45:48 -0300

    Desafío Clase 5 ItemCount
    Se crea un componente con las indicaciones del desafío ItemCount

---
+++ agzcoderhouse/laboheme/src/components/buttons/CounterTab.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {FaPlus, FaMinus} from 'react-icons/fa';
+import './Buttons.css';
+
+
+
+const CounterTab = ({price, onStock, counter, plus, minus, subtotal}) => {
+    return (
+        <div className='counterBox'>
+        <button className="counterBox__button" onClick={()=>{minus(); subtotal(counter , price);}}><FaMinus/></button>
+        <p className="counterBox__num">{counter}</p>
+        <button className="counterBox__button" onClick={()=>{plus(onStock); subtotal(counter , price); }}><FaPlus/></button>
+        </div>
+    );
+}
+
+export default CounterTab
